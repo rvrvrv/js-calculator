bd4f64a13957fe73c1230013592ac553676e37f0
rvrvrv <ibanez19@sbcglobal.net>
2017-09-08 15:42:24 -0500

    Refactor code

--- js/index.js
+++ js/index.js
@@ -1,82 +1,76 @@
-/* jshint browser: true, esversion: 6 */
-/* global $, console */
+let screen = '';
+let lastChar = '';
+let answer = '';
+const mathOp = /\+×÷-/;
 
-//Global vars
-var scrn, lastChar, lastOper, answer;
-var mathOp = /[\÷\+\-\×]/;
+// Number Buttons
+$('.numBtn').click(function () {
+  const numHit = $(this).attr('value');
+  screen = $('.screen').text();
+  // Ensure there is enough space and no leading zeroes
+  if (screen.length < 11 && screen !== '0') {
+    $('.screen').append(numHit);
+  }
+});
 
-$(document).ready(() => {
+// Decimal Button
+$('.decBtn').click(() => {
+  screen = $('.screen').text();
+  // If no decimals, add one
+  if (screen.length < 10 && !screen.includes('.')) {
+    $('.screen').append('.');
+  } else if (
+    screen.match(mathOp) &&
+      screen.lastIndexOf('.') < screen.search(mathOp)
+  ) {
+    // If only one decimal before operator, allow another
+    $('.screen').append('.');
+  }
+});
 
-	resetVars();
+// All-Clear Button
+$('#btnAC').click(() => {
+  $('.screen').text('');
+  screen = '';
+  lastChar = '';
+  answer = '';
+});
 
-	//Reset global vars
-	function resetVars() {
-		scrn = '';
-		lastChar = '';
-		lastOper = '';
-		answer = '';
-	}
+// Clear-Entry Button
+$('#btnCE').click(() => {
+  $('.screen').text($('.screen').text().slice(0, -1));
+});
 
-	//Check for blank screen. If not blank, update lastChar
-	function checkBlank() {
-		if (scrn === '') return true;
-		lastChar = scrn[scrn.length - 1];
-		return false;
-	}
+// Operator Buttons
+$('.opBtn').click(function () {
+  screen = $('.screen').text();
+  // If screen is blank, don't do anything
+  if (!screen) return;
+  // Otherwise, get last character for upcoming test
+  lastChar = screen[screen.length - 1];
+  // Ensure screen isn't full and last character isn't an operator or decimal
+  if (screen.length < 10 && !lastChar.match(mathOp) && lastChar !== '.') {
+    $('.screen').append($(this).attr('value'));
+  } else if (lastChar.match(mathOp)) {
+    // If last character is an operator, change it to button pressed
+    const lastOper = $(this).attr('value');
+    $('.screen').text(screen.replace(/.$/, lastOper));
+  }
+});
 
-	//Update screen var when any button is clicked
-	$('button').click(() => scrn = $('.screen').text());
-
-	//Number Buttons
-	$('.numBtn').click(function () {
-		let numHit = $(this).attr('value');
-		//Check for excessive leading zeroes before appending number to screen
-		if (numHit === '0' && scrn.length === 1 && scrn.includes('0')) return;
-		else if (scrn.length < 11) $('.screen').append(numHit);
-	});
-
-	//Decimal Button
-	$('.decBtn').click(function () {
-		//If no decimals or only one decimal before operator, add one
-		if ((scrn.length < 10 && !scrn.includes('.')) || mathOp.test(scrn) && (scrn.lastIndexOf('.') < scrn.search(mathOp)))
-			$('.screen').append('.');
-	});
-
-	//All-Clear Button
-	$('#btnAC').click(() => {
-		$('.screen').text('');
-		resetVars();
-	});
-
-	//Clear-Entry Button
-	$('#btnCE').click(() => $('.screen').text(scrn.slice(0, -1)));
-
-	//Operator Buttons
-	$('.opBtn').click(function () {
-		if (checkBlank()) return;
-		//Ensure screen isn't full and last character isn't an operator or decimal
-		if (scrn.length < 10 && lastChar.search(mathOp) === -1 && lastChar !== '.') {
-			lastOper = $(this).attr('value');
-			$('.screen').append(lastOper);
-		}
-		//If last character is an operator, change it to the last button pressed
-		else if (mathOp.test(lastChar)) {
-			lastOper = $(this).attr('value');
-			$('.screen').text(scrn.slice(0, -1) + lastOper);
-		}
-	});
-
-	$('.eqBtn').click(() => {
-		if (checkBlank()) return;
-		//If last character isn't an operator & user isn't dividing by zero,
-		//evaluate the expression
-		if (!mathOp.test(lastChar) && (scrn.substr(-2, 2) !== '÷0')) {
-			//Convert '×' to '*' and '÷' to '/' for eval function
-			let expression = scrn.replace(/×/g, '*');
-			expression = expression.replace(/÷/g, '/');
-			answer = parseFloat(eval(expression).toFixed(3));
-			$('.screen').text(answer);
-		}
-
-	});
+$('.eqBtn').click(() => {
+  screen = $('.screen').text();
+  // If screen is blank, don't do anything
+  if (!screen) return;
+  // Get last character for upcoming test
+  lastChar = screen[screen.length - 1];
+  // If last character isn't an operator & user isn't dividing by zero,
+  // evaluate the expression
+  if (!lastChar.match(mathOp) && !screen.includes('÷0')) {
+    // Convert '×' to 'x' and '÷' to '/' for eval function
+    let expression = screen.replace(/×/g, '*');
+    expression = expression.replace(/÷/g, '/');
+    answer = parseFloat(eval(expression).toFixed(3));
+    $('.screen').text(answer);
+  }
 });
